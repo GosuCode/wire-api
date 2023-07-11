a0e9278f549f7942e49f43ceca307ff085281df0
GosuCode <gosucode1945@gmail.com>
2023-07-11 22:48:02 +0545

    first commit

---
+++ routes/Users.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const { Users } = require("../models");
+const bcrypt = require("bcrypt");
+
+const { sign } = require("jsonwebtoken");
+
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    Users.create({
+      username: username,
+      password: hash,
+    });
+    res.json("SUCCESS");
+  });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await Users.findOne({ where: { username: username } });
+
+  if (!user) res.json({ error: "User Doesn't Exist" });
+
+  bcrypt.compare(password, user.password).then(async (match) => {
+    if (!match) res.json({ error: "Wrong Username And Password Combination" });
+
+    const accessToken = sign(
+      { username: user.username, id: user.id },
+      "importantsecret"
+    );
+    res.json(accessToken);
+  });
+});
+
+module.exports = router;
